e40f44c37898631bf1470489f95b9569a40e54a2
chenyh <917029221@qq.com>
2021-01-07 11:45:10 +0800

    feat:登入登出接口实现

--- routes/user.js
+++ routes/user.js
@@ -3,21 +3,29 @@ const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 router.prefix('/api/user')
+const { awaitWrap } = require('../utils/awaitWrap');
 
 router.post('/login', async function (ctx, next) {
     const { username, password } = ctx.request.body
-    const data = await login(username, password)
-    if (data.username) {
+    const [err, data] = await awaitWrap(login(username, password))
+    if (data && data.username) {
         // 设置 session
+        // console.log("session:",ctx.ession)
         ctx.session.username = data.username
-        ctx.session.realname = data.realname
-
-        ctx.body = new SuccessModel()
-        return
+        ctx.body = new SuccessModel({
+            username:ctx.session.username
+        })
+        return 
     }
     ctx.body = new ErrorModel('登录失败')
 })
+router.get('/logout', async function (ctx, next) {
+    ctx.session.username = undefined
+    ctx.body = new SuccessModel()
+    return 
+})
 
+// 统计文章数
 // router.get('/session-test', async function (ctx, next) {
 //   if (ctx.session.viewCount == null) {
 //     ctx.session.viewCount = 0
@@ -29,5 +37,18 @@ router.post('/login', async function (ctx, next) {
 //     viewCount: ctx.session.viewCount
 //   }
 // })
+
+
+router.get('/session-test', async function (ctx, next) {
+    if (ctx.session.viewCount == null) {
+      ctx.session.viewCount = 0
+    }
+    ctx.session.viewCount++
+  
+    ctx.body ={
+      errno: 0,
+      viewCount: ctx.session.viewCount
+    }
+  })
 
 module.exports = router
