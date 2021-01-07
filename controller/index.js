7c5641d61745454891303f410899b9430d9a9b7b
chenyh <917029221@qq.com>
2021-01-07 12:20:11 +0800

    bugfix: mongoose estimatedDocumentCount 计数条件不生效改用countDocuments

--- controller/index.js
+++ controller/index.js
@@ -4,8 +4,8 @@ const Walking = require('../db/models/Walking')
 // const Subscribe = require('../db/models/Subscribe')
 
 const getCounts = async () => { 
-    let blogNum = await Blog.estimatedDocumentCount({status:1})
-    let walkingNum = await Walking.estimatedDocumentCount({status:1})
+    let blogNum = await Blog.countDocuments({"status":1})
+    let walkingNum = await Walking.countDocuments({"status":1})
     let subscribeNum  = 0
     return {
         blogNum,
